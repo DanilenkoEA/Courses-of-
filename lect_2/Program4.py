9fbbdf1f15355339c3f735bec775fdf9cefd0c7a
Elena Danilenko <danilenkoea@gmail.com>
2016-10-09 19:24:38 +0300

    Update hw2.4

--- lect_2/Program4.py
+++ lect_2/Program4.py
@@ -1,12 +1,25 @@
 # coding: utf-8
 a1 = 5
 a2 = 2
-a3 = 6
+a3 = 8
 a4 = 7
 a5 = 1
-maximum = 8
-if maximum < max(a1, a2, a3, a4, a5):
+maximum = 6
+
+if maximum < a1:
+    maximum = a1
     print maximum
-elif maximum == max(a1, a2, a3, a4, a5):
+elif maximum < a2:
+    maximum = a2
+    print maximum
+elif maximum < a3:
+    maximum = a3
+    print maximum
+elif maximum < a4:
+    maximum = a4
+    print maximum
+elif maximum < a5:
+    maximum = a5
     print maximum
 else:
+    print maximum
