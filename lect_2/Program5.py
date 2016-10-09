53e0d347f8592437327ba7c73dab9f8df51f3938
Elena Danilenko <danilenkoea@gmail.com>
2016-10-09 21:23:00 +0300

    Update hw2.5

--- lect_2/Program5.py
+++ lect_2/Program5.py
@@ -1,11 +1,32 @@
 # coding: utf-8
-a1 = 5
-a2 = 2
+a1 = 6
+a2 = 1
 a3 = 6
 a4 = 7
 a5 = 1
-counter = 1
-if abs(counter) > 5:
-    print counter + 1
-else:
+counter = 0
+if a1 > 5:
+    counter += 1
     print counter
+else:
+    pass
+if a2 > 5:
+    counter += 1
+    print counter
+else:
+    pass
+if a3 > 5:
+    counter += 1
+    print counter
+else:
+    pass
+if a4 > 5:
+    counter += 1
+    print counter
+else:
+    pass
+if a5 > 5:
+    counter += 1
+    print counter
+else:
+    pass
